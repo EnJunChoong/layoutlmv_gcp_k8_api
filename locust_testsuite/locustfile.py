e856e8a579f1369ade24b41d6324d58973e5aa54
enjunz <enjunz@DESKTOP-AKP7GB4.localdomain>
2022-10-10 23:24:12 +0800

    add testsuite using locust for simulating request

---
+++ locust_testsuite/locustfile.py
@@ -0,0 +1,25 @@
+import os, mimetypes
+from PIL import Image
+from locust import HttpUser, between, task
+
+print("enter token")
+secret_key=input()
+
+class test_inference_image(HttpUser):
+    wait_time = between(5,8)
+    
+    def on_start(self):
+        self.headers={"Authorization": f"Bearer {secret_key}"}       
+
+        path_to_image="test.png"
+        with open(path_to_image, "rb") as f:
+            self.bytes_obj = f.read()
+            self.fname = os.path.basename(path_to_image)
+            self.content_type = mimetypes.guess_type(path_to_image)[0]
+    
+    @task
+    def inference_image(self):
+        self.client.post("/inference_image/", headers=self.headers, files={"file": ( self.fname, self.bytes_obj, self.content_type)})
+    # @task
+    # def docs(self):
+    #     self.client.get("/docs")
